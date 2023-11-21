0decaa56b077f8ec0f09078fcaa05a528945b3ef
Chandrashekhar B <115780527+chandrashekharb369@users.noreply.github.com>
2023-11-21 21:06:06 +0530

    sorce.py

--- sorce.py
+++ sorce.py
@@ -1,42 +1,47 @@
+import tkinter as tk
+from tkinter import messagebox
 import pygame
 import time
 import random
 
 pygame.init()
 
+
+dis_width = 800
+dis_height = 600
+dis = pygame.display.set_mode((dis_width, dis_height))
+pygame.display.set_caption('Snake Game')
+clock = pygame.time.Clock()
+
+
 white = (255, 255, 255)
-yellow = (255, 255, 102)
 black = (0, 0, 0)
 red = (213, 50, 80)
 green = (0, 255, 0)
 blue = (50, 153, 213)
 
-dis_width = 800
-dis_height = 600
-
-dis = pygame.display.set_mode((dis_width, dis_height))
-pygame.display.set_caption('Snake Game')
-
-clock = pygame.time.Clock()
 
 snake_block = 10
-snake_speed = 30
 
 font_style = pygame.font.SysFont(None, 50)
 score_font = pygame.font.SysFont(None, 35)
 
 
-def our_snake(snake_block, snake_list):
-    for x in snake_list:
-        pygame.draw.rect(dis, black, [x[0], x[1], snake_block, snake_block])
-
+speed_levels = {"Easy": 15, "Normal": 30, "Hard": 45}
 
 def message(msg, color):
     mesg = font_style.render(msg, True, color)
     dis.blit(mesg, [dis_width / 6, dis_height / 3])
 
+def Your_score(score):
+    value = score_font.render("Your Score: " + str(score), True, white)
+    dis.blit(value, [0, 0])
 
-def game_loop():
+def our_snake(snake_block, snake_list):
+    for x in snake_list:
+        pygame.draw.rect(dis, black, [x[0], x[1], snake_block, snake_block])
+
+def game_loop(speed):
     game_over = False
     game_close = False
 
@@ -53,10 +58,10 @@ def game_loop():
     foody = round(random.randrange(0, dis_height - snake_block) / 10.0) * 10.0
 
     while not game_over:
-
-        while game_close == True:
+        while game_close:
             dis.fill(blue)
             message("You lost! Press Q-Quit or C-Play Again", red)
+            Your_score(Length_of_snake - 1)
             pygame.display.update()
 
             for event in pygame.event.get():
@@ -65,7 +70,7 @@ def game_loop():
                         game_over = True
                         game_close = False
                     if event.key == pygame.K_c:
-                        game_loop()
+                        game_loop(speed)
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -102,6 +107,7 @@ def game_loop():
                 game_close = True
 
         our_snake(snake_block, snake_List)
+        Your_score(Length_of_snake - 1)
 
         pygame.display.update()
 
@@ -110,10 +116,37 @@ def game_loop():
             foody = round(random.randrange(0, dis_height - snake_block) / 10.0) * 10.0
             Length_of_snake += 1
 
-        clock.tick(snake_speed)
+        clock.tick(speed)
 
     pygame.quit()
     quit()
 
 
-game_loop()
+def start_game(speed):
+    root.destroy()
+    game_loop(speed_levels[speed])
+
+def show_speed_level():
+    speed = speed_var.get()
+    speed_label.config(text=f"Selected Speed Level: {speed}")
+
+
+root = tk.Tk()
+root.title("Snake Game - Speed Levels")
+
+speed_var = tk.StringVar()
+speed_var.set("Normal")
+
+speed_label = tk.Label(root, text="Selected Speed Level: Normal")
+speed_label.pack(pady=10)
+
+speed_menu = tk.OptionMenu(root, speed_var, *speed_levels.keys())
+speed_menu.pack(pady=10)
+
+start_button = tk.Button(root, text="Start Game", command=lambda: start_game(speed_var.get()))
+start_button.pack(pady=10)
+
+show_speed_button = tk.Button(root, text="Show Speed Level", command=show_speed_level)
+show_speed_button.pack(pady=10)
+
+root.mainloop()
